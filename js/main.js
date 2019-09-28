8aa6abe506302353112827fa26e26f5677b1e6aa
dnguy24 <dnguy24@u.rochester.edu>
2019-09-28 00:35:23 -0400

    update

--- js/main.js
+++ js/main.js
@@ -11,16 +11,14 @@ var rect = c.getBoundingClientRect();
 var x = y = 0;
 var lastmouseX = lastmouseY = 0;
 var features = [
-    [97, 77],
-    [96, 156],
-    [108, 221],
-    [121, 308],
-    [200, 91],
-    [218, 173],
-    [229, 234],
-    [255, 315]
+    [147, 78],
+    [307, 73],
+    [200, 188],
+    [297, 251],
+    [364, 310],
+    [407, 167]
 ];
-var labels = [-1, -1, -1, -1, 1, 1, 1, 1];
+var labels = [-1, -1, -1, 1, 1, 1];
 var start1 = false;
 var start2 = false;
 c.addEventListener("mousedown", function(e){
@@ -88,38 +86,109 @@ function getCol(matrix, col){
     }
     return column;
 }
-function svm(){
-    var svm = new svmjs.SVM();
-    svm.train(features, labels);
-    console.log(svm.getWeights());
-    console.log(svm.predict(features));
-    var weights = svm.getallW();
-    console.log(weights);
-    console.log(typeof weights[0])
 
-
-    for(var i=0; i<c.clientWidth; i++){
-        for(var j=0; j<c.clientHeight; j++){
-            var z = svm.predictOne([i, j]);
-            if(z==-1){
-                ctx.fillStyle = "LightCoral";
-                ctx.fillRect(i, j, 1,1);
-            }else if(z==1){
-                ctx.fillStyle = "LightGreen";
-                ctx.fillRect(i, j, 1,1);
+function cut(){
+    sm = create(features);
+    gameLoop();
+    function gameLoop(){
+        if(sm.x < sm.limit-1){
+            console.log(sm.x)
+            window.requestAnimationFrame(gameLoop);
+            sm.drawsvm();
+            sm.update();
+        }else{
+        console.log("drawing best");
+        ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+        var loz = [];    
+        for(var i=0; i<c.clientWidth; i++){
+            for(var j=0; j<c.clientHeight; j++){
+                loz.push([i,j]);
             }
         }
-    }
-    for(var i=0, len=features.length; i<len; i++){
-        if(labels[i]==1){
-            console.log("drawing");
-            drawCircle(features[i][0], features[i][1], "green");
-        }else if(labels[i]==-1){
-            console.log("drawing");
-            drawCircle(features[i][0], features[i][1], "red");
+        var results = sm.svm.predict(loz);
+        for(var i=0; i<loz.length; i++){
+            var z = results[i];
+            if(z==-1){
+                ctx.fillStyle = "LightCoral";
+                ctx.fillRect(loz[i][0], loz[i][1], 1,1);
+            }else if(z==1){
+                ctx.fillStyle = "LightGreen";
+                ctx.fillRect(loz[i][0], loz[i][1], 1,1);
+            }
+        }
+        for(var i=0, len=features.length; i<len; i++){
+            if(labels[i]==1){
+                drawCircle(features[i][0], features[i][1], "green");
+            }else if(labels[i]==-1){
+                drawCircle(features[i][0], features[i][1], "red");
+            }
+        }
+        console.log("done");
         }
     }
 }
+
+function create(features){
+    var that = {};
+    var svm = new svmjs.SVM();
+    svm.train(features, labels, {kernel: 'rbf', rbfsigma: 20, C:1});
+    console.log(features);
+    console.log(svm.getWeights());
+    console.log(svm.predict(features));
+    var weights = svm.getallW();
+    console.log(weights.length);
+    console.log(weights);
+    var tickperframe = 1;
+    var tickcount = 0;
+    var x = 0;
+    that.limit = weights.length;
+    that.x = x;
+    that.svm = svm;
+    that.drawsvm = function(){
+
+        ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);    
+        var weights = svm.getallW();
+        var alphas = svm.getallAlpha();
+        for(var i=0; i<c.clientWidth; i++){
+            for(var j=0; j<c.clientHeight; j++){
+                var z = svm.fakepredictOne(weights[x], alphas[x], [i, j]);
+                if(z==-1){
+                    ctx.fillStyle = "LightCoral";
+                    ctx.fillRect(i, j, 1,1);
+                }else if(z==1){
+                    ctx.fillStyle = "LightGreen";
+                    ctx.fillRect(i, j, 1,1);
+                }
+            }
+        }
+        for(var i=0, len=features.length; i<len; i++){
+            if(labels[i]==1){
+                drawCircle(features[i][0], features[i][1], "green");
+            }else if(labels[i]==-1){
+                drawCircle(features[i][0], features[i][1], "red");
+            }
+        }
+        console.log(weights[x]);
+    }
+
+    that.update = function(){
+        that.x = x;
+        tickcount+=1;
+        if(tickcount > tickperframe && x < svm.getallW().length){
+            tickcount = 0;
+            x+=1;
+        }
+    }
+    return that;
+}
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 function draw(val){
     ctx.fillStyle = val;
     ctx.fillRect(x, y, 10, 10);
